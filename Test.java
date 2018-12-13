a4efb91305906a13b087c154a1fe91748e5f9447
Skaaia <42056349+Skaaia@users.noreply.github.com>
2018-12-13 14:20:19 +0200

    Delete Test.java

--- Test.java
+++
@@ -1,8 +0,0 @@
-package com.skaaria;
-
-public class Test {
-    public static void main(String[] args) throws Exception {
-        User user1=new User("123","qwerty","qwert","Ivan","Ivanov",17);
-        user1.validator();
-    }
-}
